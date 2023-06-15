5b513110b6e92e3c3cfae32b4ad98d1bb95dc9b6
HE Shi-Jun <hax@heshijun.net>
2023-06-15 19:19:55 +0800

    fix: cloned node should be synthesized

--- src/clone-node.ts
+++ src/clone-node.ts
@@ -1,14 +1,11 @@
 import ts from "typescript";
 
-type Mutable<T> = { -readonly [K in keyof T]: T[K] };
+// Typescript undocumented API, see Ron Buckton's explaination why it's not public:
+// https://github.com/microsoft/TypeScript/issues/40507#issuecomment-737628756
+// But it's very fit our case
+const { cloneNode: _cloneNode } = ts.factory as any;
 
 export function cloneNode<T extends ts.Node>(node: T): T {
-  const props = { ...node } as Mutable<T>;
-  props.pos = -1;
-  props.end = -1;
-  const base = new (node as any).constructor();
-  Object.keys(props).forEach(k => {
-    base[k] = props[k as keyof T];
-  });
-  return base as T;
+  const cloned = _cloneNode(node);
+  return ts.setOriginalNode(cloned, undefined); // remove original
 }
